c47aff5095b6efcd52e8ad946e66c103a671d384
unknown <fachry.classroom@gmail.com>
2026-02-28 22:46:47 +0800

    UP POSTTEST 1

---
+++ POSTTEST_1/WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        leflef = New Label()
+        Label2 = New Label()
+        lblIPK = New Label()
+        lblPredikat = New Label()
+        txtIPS = New TextBox()
+        btnTambah = New Button()
+        btnReset = New Button()
+        SuspendLayout()
+        ' 
+        ' leflef
+        ' 
+        leflef.AutoSize = True
+        leflef.Location = New Point(133, 101)
+        leflef.Name = "leflef"
+        leflef.Size = New Size(37, 25)
+        leflef.TabIndex = 0
+        leflef.Text = "IPS"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(133, 208)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(37, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "IPK"
+        ' 
+        ' lblIPK
+        ' 
+        lblIPK.AutoSize = True
+        lblIPK.Location = New Point(197, 208)
+        lblIPK.Name = "lblIPK"
+        lblIPK.Size = New Size(46, 25)
+        lblIPK.TabIndex = 2
+        lblIPK.Text = "0.00"
+        ' 
+        ' lblPredikat
+        ' 
+        lblPredikat.AutoSize = True
+        lblPredikat.Location = New Point(197, 252)
+        lblPredikat.Name = "lblPredikat"
+        lblPredikat.Size = New Size(19, 25)
+        lblPredikat.TabIndex = 3
+        lblPredikat.Text = "-"
+        ' 
+        ' txtIPS
+        ' 
+        txtIPS.Location = New Point(197, 101)
+        txtIPS.Name = "txtIPS"
+        txtIPS.Size = New Size(230, 31)
+        txtIPS.TabIndex = 4
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(197, 138)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(112, 34)
+        btnTambah.TabIndex = 5
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(315, 138)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(112, 34)
+        btnReset.TabIndex = 6
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
+        Controls.Add(txtIPS)
+        Controls.Add(lblPredikat)
+        Controls.Add(lblIPK)
+        Controls.Add(Label2)
+        Controls.Add(leflef)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents leflef As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblIPK As Label
+    Friend WithEvents lblPredikat As Label
+    Friend WithEvents txtIPS As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+
+End Class
---
+++ POSTTEST_1/WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+
+    ' Variabel penyimpanan
+    Dim totalNilai As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+
+        Dim ips As Double
+
+        ' Validasi input angka
+        If Double.TryParse(txtIPS.Text, ips) Then
+
+            ' Validasi rentang 0 - 4
+            If ips < 0 Or ips > 4 Then
+                MessageBox.Show("IPS harus antara 0 sampai 4")
+                Exit Sub
+            End If
+
+            ' Tambah data
+            totalNilai = totalNilai + ips
+            jumlahSemester = jumlahSemester + 1
+
+            ' Hitung IPK
+            Dim ipk As Double = totalNilai / jumlahSemester
+
+            ' Tampilkan IPK
+            lblIPK.Text = ipk.ToString("0.00")
+
+            ' Tentukan predikat
+            If ipk >= 3.01 Then
+                lblPredikat.Text = "Sangat Memuaskan"
+            ElseIf ipk >= 2.76 Then
+                lblPredikat.Text = "Memuaskan"
+            ElseIf ipk >= 2.0 Then
+                lblPredikat.Text = "Cukup"
+            Else
+                lblPredikat.Text = "-"
+            End If
+
+            ' Kosongkan input
+            txtIPS.Clear()
+
+        Else
+            MessageBox.Show("Masukkan angka yang benar")
+        End If
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        ' Reset semua nilai
+        totalNilai = 0
+        jumlahSemester = 0
+
+        lblIPK.Text = "0.00"
+        lblPredikat.Text = "-"
+        txtIPS.Clear()
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles leflef.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
